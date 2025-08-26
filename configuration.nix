2131d425fb0045e6fe4b6445f1132800fd067496
Sahaj Jain <jainsahaj@gmail.com>
2025-08-26 21:52:40 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,9 +3,8 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./virtualization.nix
   ];
-  
+
   users.users.spawnhouse = {
     isNormalUser = true;
     extraGroups = [
@@ -18,7 +17,15 @@
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmQSQt8pJAuVrlfPSwMpjyrwtRrZhhv/mKNaW9PYCJz4TUaOEIRLDyVrWZlOSJlcfRxnxlBSg6QXqeUphYVe6SvES+cg7NYCLPK3YjWVEGe2YI+FeMhBUJIqjTyylNY1NY3aq6Q7mrT7cT0rqLtIdTk7DiVEsrINWg/yT+CAG9KbWuk+/aNXpGdPNfMJkHzt/25wCPpoOP2ByxbKKnH6qBWpnzZn/xbhm0XIZYxqc6iklVsCFIs2E2gvH1NINniuOgUsReWCrnFigEhH8P5V90Qxwr/65ttakNSV4SEnDFEMecGk9qAlKrg+N8oLQrLh1+Bs0f5NOKLlP7m+FmR6sV imported-openssh-key" # Replace with your actual public key
     ];
+    hashedPassword = "!";
   };
+
+  security.sudo.extraRules = [{
+    users = [ "spawnhouse" ];
+    commands = [
+      { command = "ALL"; options = [ "NOPASSWD" ]; }
+    ];
+  }];
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmQSQt8pJAuVrlfPSwMpjyrwtRrZhhv/mKNaW9PYCJz4TUaOEIRLDyVrWZlOSJlcfRxnxlBSg6QXqeUphYVe6SvES+cg7NYCLPK3YjWVEGe2YI+FeMhBUJIqjTyylNY1NY3aq6Q7mrT7cT0rqLtIdTk7DiVEsrINWg/yT+CAG9KbWuk+/aNXpGdPNfMJkHzt/25wCPpoOP2ByxbKKnH6qBWpnzZn/xbhm0XIZYxqc6iklVsCFIs2E2gvH1NINniuOgUsReWCrnFigEhH8P5V90Qxwr/65ttakNSV4SEnDFEMecGk9qAlKrg+N8oLQrLh1+Bs0f5NOKLlP7m+FmR6sV imported-openssh-key"
@@ -26,11 +33,37 @@
 
   environment.systemPackages = with pkgs; [
     wget
+    git
+    unzip
+    lsof
   ];
 
-  networking.firewall.allowedUDPPorts = [ 8211 ];
+  nixpkgs.config.allowUnfree = true;
 
-  services.openssh.enable = true;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 443 ];
+    allowedUDPPorts = [];
+  };
+
+  services.openssh = {
+  enable = true;
+  settings = {
+      PasswordAuthentication = false;
+    };
+  };
+
+  virtualisation.docker = {
+    enable = true;
+    rootless.enable = true;
+  };
+
+  environment.shellAliases = {
+    nswitch = "sudo nixos-rebuild switch";
+    nconfig = "sudo nano /etc/nixos/configuration.nix";
+  };
+
+  networking.hostName = "nixos";
 
   time.timeZone = "Asia/Kolkata";
 
