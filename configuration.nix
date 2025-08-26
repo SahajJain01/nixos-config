03e2ba0b2a62678a13e95a211009cd2913ebeed6
Sahaj Jain <jainsahaj@gmail.com>
2025-08-26 22:34:23 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -5,70 +5,110 @@
     ./hardware-configuration.nix
   ];
 
+  ############################
+  # USERS
+  ############################
   users.users.spawnhouse = {
     isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "docker"
-    ];
-    packages = with pkgs; [
-      tree
-    ];
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [ tree ];
     openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmQSQt8pJAuVrlfPSwMpjyrwtRrZhhv/mKNaW9PYCJz4TUaOEIRLDyVrWZlOSJlcfRxnxlBSg6QXqeUphYVe6SvES+cg7NYCLPK3YjWVEGe2YI+FeMhBUJIqjTyylNY1NY3aq6Q7mrT7cT0rqLtIdTk7DiVEsrINWg/yT+CAG9KbWuk+/aNXpGdPNfMJkHzt/25wCPpoOP2ByxbKKnH6qBWpnzZn/xbhm0XIZYxqc6iklVsCFIs2E2gvH1NINniuOgUsReWCrnFigEhH8P5V90Qxwr/65ttakNSV4SEnDFEMecGk9qAlKrg+N8oLQrLh1+Bs0f5NOKLlP7m+FmR6sV imported-openssh-key" # Replace with your actual public key
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmQSQt8pJAuVrlfPSwMpjyrwtRrZhhv/mKNaW9PYCJz4TUaOEIRLDyVrWZlOSJlcfRxnxlBSg6QXqeUphYVe6SvES+cg7NYCLPK3YjWVEGe2YI+FeMhBUJIqjTyylNY1NY3aq6Q7mrT7cT0rqLtIdTk7DiVEsrINWg/yT+CAG9KbWuk+/aNXpGdPNfMJkHzt/25wCPpoOP2ByxbKKnH6qBWpnzZn/xbhm0XIZYxqc6iklVsCFIs2E2gvH1NINniuOgUsReWCrnFigEhH8P5V90Qxwr/65ttakNSV4SEnDFEMecGk9qAlKrg+N8oLQrLh1+Bs0f5NOKLlP7m+FmR6sV imported-openssh-key"
     ];
     hashedPassword = "!";
   };
 
   security.sudo.extraRules = [{
     users = [ "spawnhouse" ];
-    commands = [
-      { command = "ALL"; options = [ "NOPASSWD" ]; }
-    ];
+    commands = [{ command = "ALL"; options = [ "NOPASSWD" ]; }];
   }];
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmQSQt8pJAuVrlfPSwMpjyrwtRrZhhv/mKNaW9PYCJz4TUaOEIRLDyVrWZlOSJlcfRxnxlBSg6QXqeUphYVe6SvES+cg7NYCLPK3YjWVEGe2YI+FeMhBUJIqjTyylNY1NY3aq6Q7mrT7cT0rqLtIdTk7DiVEsrINWg/yT+CAG9KbWuk+/aNXpGdPNfMJkHzt/25wCPpoOP2ByxbKKnH6qBWpnzZn/xbhm0XIZYxqc6iklVsCFIs2E2gvH1NINniuOgUsReWCrnFigEhH8P5V90Qxwr/65ttakNSV4SEnDFEMecGk9qAlKrg+N8oLQrLh1+Bs0f5NOKLlP7m+FmR6sV imported-openssh-key"
   ];
 
+  ############################
+  # PACKAGES / TOOLS
+  ############################
   environment.systemPackages = with pkgs; [
-    wget
-    git
-    unzip
-    lsof
+    wget git unzip lsof
+    curl htop
+    whois # mkpasswd for bcrypt
   ];
-
   nixpkgs.config.allowUnfree = true;
 
+  ############################
+  # NETWORK / FIREWALL / SSH
+  ############################
+  networking.hostName = "nixos";
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 443 ];
-    allowedUDPPorts = [];
+    allowedTCPPorts = [ 22 443 8443 ]; # 22 for SSH, 8443 for code-server, 443 if you later reverse-proxy
+    allowedUDPPorts = [ ];
   };
 
   services.openssh = {
-  enable = true;
-  settings = {
+    enable = true;
+    settings = {
       PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      X11Forwarding = false;
     };
   };
 
-  virtualisation.docker = {
+  ############################
+  # DEV EXPERIENCE
+  ############################
+  services.code-server = {
     enable = true;
-    rootless.enable = true;
+    user = "spawnhouse";
+    host = "0.0.0.0";
+    port = 8443;
+    auth = "password";
+    # Generate with: mkpasswd -m bcrypt
+    hashedPassword = "$2b$05$yurNJ8IIqrz8a64VPGdKgOyJhW.glo7tYAAN00bnuqaXZCsmO9/t2";
+    extensions = [
+      "esbenp.prettier-vscode"
+      "eamodio.gitlens"
+      "mhutchie.git-graph"
+      "dracula-theme.theme-dracula"
+    ];
   };
 
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+
+  ############################
+  # SHELL ALIASES
+  ############################
   environment.shellAliases = {
     nswitch = "sudo nixos-rebuild switch";
     nconfig = "sudo nano /etc/nixos/configuration.nix";
   };
 
-  networking.hostName = "nixos";
-
+  ############################
+  # LOCALE / BOOT
+  ############################
   time.timeZone = "Asia/Kolkata";
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  ############################
+  # NIX SETTINGS (flakes support)
+  ############################
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://cache.nix-community.org"
+      "https://cache.garnix.io"
+    ];
+    trusted-public-keys = [
+      "cache.nix-community.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2LJZ7Y2l3k="
+    ];
+  };
 
   system.stateVersion = "25.05";
 }
