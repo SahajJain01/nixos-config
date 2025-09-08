559057e0aeb319375e133b6605b0d09eba0854b5
Sahaj Jain <jainsahaj@gmail.com>
2025-09-08 12:49:39 +0530

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -32,11 +32,16 @@
     isNormalUser = true;
     description = "Deployment user for GitHub Actions";
     home = "/home/github";
+    createHome = true;
+    linger = true;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmQSQt8pJAuVrlfPSwMpjyrwtRrZhhv/mKNaW9PYCJz4TUaOEIRLDyVrWZlOSJlcfRxnxlBSg6QXqeUphYVe6SvES+cg7NYCLPK3YjWVEGe2YI+FeMhBUJIqjTyylNY1NY3aq6Q7mrT7cT0rqLtIdTk7DiVEsrINWg/yT+CAG9KbWuk+/aNXpGdPNfMJkHzt/25wCPpoOP2ByxbKKnH6qBWpnzZn/xbhm0XIZYxqc6iklVsCFIs2E2gvH1NINniuOgUsReWCrnFigEhH8P5V90Qxwr/65ttakNSV4SEnDFEMecGk9qAlKrg+N8oLQrLh1+Bs0f5NOKLlP7m+FmR6sV imported-openssh-key"
     ];
   };
+  systemd.tmpfiles.rules = [
+    "d /srv/apps 0755 github github -"
+  ];
 
   # Services
   services.openssh = {
